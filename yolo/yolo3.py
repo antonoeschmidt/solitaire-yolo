dac2e69edf319bc265082c43bb7e8a9f96216a77
antonoeschmidt <s163053@student.dtu.dk>
2020-06-17 13:25:24 +0200

    Refactoring

--- yolo/yolo3.py
+++ yolo/yolo3.py
@@ -76,65 +76,27 @@ def detect(image_BGR):
     # Getting list with names of all layers from YOLO v3 network
     layers_names_all = network.getLayerNames()
 
-    # Check point
-    # print()
-    # print(layers_names_all)
-
     # Getting only output layers' names that we need from YOLO v3 algorithm
     # with function that returns indexes of layers with unconnected outputs
     layers_names_output = \
         [layers_names_all[i[0] - 1] for i in network.getUnconnectedOutLayers()]
 
-    # Check point
-    # print()
-    # print(layers_names_output)  # ['yolo_82', 'yolo_94', 'yolo_106']
-
     # Setting minimum probability to eliminate weak predictions
     probability_minimum = 0.5
 
-    # Setting threshold for filtering weak bounding boxes
-    # with non-maximum suppression
+    # Setting threshold for filtering weak bounding boxes with non-maximum suppression
     threshold = 0.3
 
     # Generating colours for representing every detected object
-    # with function randint(low, high=None, size=None, dtype='l')
     colours = np.random.randint(0, 255, size=(len(labels), 3), dtype='uint8')
 
-    # # Check point
-    # print()
-    # print(type(colours))  # <class 'numpy.ndarray'>
-    # print(colours.shape)  # (80, 3)
-    # print(colours[0])  # [172  10 127]
-
-    """
-    End of:
-    Loading YOLO v3 network
-    """
-
-    """
-    Start of:
-    Implementing Forward pass
-    """
-
-    # Implementing forward pass with our blob and only through output layers
-    # Calculating at the same time, needed time for forward pass
     network.setInput(blob)  # setting blob as input to the network
     start = time.time()
     output_from_network = network.forward(layers_names_output)
     end = time.time()
 
-    # Showing spent time for forward pass
+    # Showing spent time
     print('Objects Detection took {:.5f} seconds'.format(end - start))
-
-    """
-    End of:
-    Implementing Forward pass
-    """
-
-    """
-    Start of:
-    Getting bounding boxes
-    """
 
     # Preparing lists for detected bounding boxes,
     # obtained confidences and class's number
@@ -142,35 +104,20 @@ def detect(image_BGR):
     confidences = []
     class_numbers = []
 
-    # Going through all output layers after feed forward pass
     for result in output_from_network:
-        # Going through all detections from current output layer
         for detected_objects in result:
-            # Getting 80 classes' probabilities for current detected object
+            # Getting all 80 classes' probabilities for current detected object
             scores = detected_objects[5:]
             # Getting index of the class with the maximum value of probability
             class_current = np.argmax(scores)
             # Getting value of probability for defined class
             confidence_current = scores[class_current]
 
-            # Check point
-            # Every 'detected_objects' numpy array has first 4 numbers with
-            # bounding box coordinates and rest 80 with probabilities for every class
-            # print(detected_objects.shape)  # (85,)
-
             # Eliminating weak predictions with minimum probability
             if confidence_current > probability_minimum:
-                # Scaling bounding box coordinates to the initial image size
-                # YOLO data format keeps coordinates for center of bounding box
-                # and its current width and height
-                # That is why we can just multiply them elementwise
-                # to the width and height
-                # of the original image and in this way get coordinates for center
-                # of bounding box, its width and height for original image
                 box_current = detected_objects[0:4] * np.array([w, h, w, h])
 
                 # Now, from YOLO data format, we can get top left corner coordinates
-                # that are x_min and y_min
                 x_center, y_center, box_width, box_height = box_current
                 x_min = int(x_center - (box_width / 2))
                 y_min = int(y_center - (box_height / 2))
@@ -180,52 +127,19 @@ def detect(image_BGR):
                 confidences.append(float(confidence_current))
                 class_numbers.append(class_current)
 
-    """
-    End of:
-    Getting bounding boxes
-    """
-
-    """
-    Start of:
-    Non-maximum suppression
-    """
-
     # Implementing non-maximum suppression of given bounding boxes
-    # With this technique we exclude some of bounding boxes if their
-    # corresponding confidences are low or there is another
-    # bounding box for this region with higher confidence
-
-    # It is needed to make sure that data type of the boxes is 'int'
-    # and data type of the confidences is 'float'
-    # https://github.com/opencv/opencv/issues/12789
     results = cv2.dnn.NMSBoxes(bounding_boxes, confidences,
                                probability_minimum, threshold)
 
-    """
-    End of:
-    Non-maximum suppression
-    """
-
-    """
-    Start of:
-    Drawing bounding boxes and labels
-    """
-
     # Defining counter for detected objects
     counter = 1
-
-    # Checking if there is at least one detected object after non-maximum suppression
     if len(results) > 0:
-        # Going through indexes of results
         for i in results.flatten():
             # Showing labels of the detected objects
             print('Object {0}: {1}'.format(counter, labels[int(class_numbers[i])]))
-
-            # Incrementing counter
             counter += 1
 
             # Getting current bounding box coordinates,
-            # its width and height
             x_min, y_min = bounding_boxes[i][0], bounding_boxes[i][1]
             box_width, box_height = bounding_boxes[i][2], bounding_boxes[i][3]
 
@@ -257,31 +171,9 @@ def detect(image_BGR):
     print('Total objects been detected:', len(bounding_boxes))
     print('Number of objects left after non-maximum suppression:', counter - 1)
 
-    """
-    End of:
-    Drawing bounding boxes and labels
-    """
 
     # Showing Original Image with Detected Objects
-    # Giving name to the window with Original Image
-    # And specifying that window is resizable
     cv2.namedWindow('Detections', cv2.WINDOW_NORMAL)
-    # Pay attention! 'cv2.imshow' takes images in BGR format
     cv2.imshow('Detections', image_BGR)
-    # Waiting for any key being pressed
     cv2.waitKey(0)
-    # Destroying opened window with name 'Detections'
     cv2.destroyWindow('Detections')
-
-"""
-Some comments
-
-With OpenCV function 'cv2.dnn.blobFromImage' we get 4-dimensional
-so called 'blob' from input image after mean subtraction,
-normalizing, and RB channels swapping. Resulted shape has:
- - number of images
- - number of channels
- - width
- - height
-E.G.: blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size, mean, swapRB=True)
-"""
