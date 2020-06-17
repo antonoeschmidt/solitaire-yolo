f5e239e43394d699ad9143c192f8a91485e60461
antonoeschmidt <s163053@student.dtu.dk>
2020-06-17 15:13:32 +0200

    Flask initiated

--- flask/pyServer.py
+++ flask/pyServer.py
@@ -1,6 +1,12 @@
+from imageproc import chuckify
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/test')
+@app.route('/test', methods=['GET'])
 def hello_world():
+    # get picture from IMGUR
+
+    #chuckify(image)
+    cards = []
+
     return 'Hello, World!'
