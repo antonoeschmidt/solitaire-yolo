221d627513963431b664781d711049577779fdef
antonoeschmidt <s163053@student.dtu.dk>
2020-06-24 16:02:22 +0200

    Imports and init.py

--- main/flask/__init__.py
+++ main/flask/__init__.py
@@ -1,3 +1,6 @@
-import main.yolo.imageproc
+from main.yolo.imageproc import chuckify
+import cv2
 
+img = cv2.imread('../images/IMG_1488.JPG')
+chuckify(img)
 print('loaded')
--- main/flask/pyServer.py
+++ main/flask/pyServer.py
@@ -1,13 +1,3 @@
-# MODULE_PATH = "/Users/antonoeschmidt/PycharmProjects/solitaire-yolo/yolo/__init__.py"
-# MODULE_PATH = "/home/antonio/solitaire-yolo/yolo/__init__.py"
-# MODULE_NAME = "yolo"
-# import importlib.util
-# import sys
-# spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
-# module = importlib.util.module_from_spec(spec)
-# sys.modules[spec.name] = module
-# spec.loader.exec_module(module)
-
 import sys
 # sys.path.append("/Users/antonoeschmidt/PycharmProjects/solitaire-yolo/yolo")
 # sys.path.append("/home/antonio/solitaire-yolo/yolo")
