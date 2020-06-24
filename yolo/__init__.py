b301fea21495d8ed01a0d3fe2d46b9a4d2a1a7b9
antonoeschmidt <s163053@student.dtu.dk>
2020-06-24 14:41:11 +0200

    Imports and init.py

--- yolo/__init__.py
+++ yolo/__init__.py
@@ -1,1 +1,10 @@
-from card import Card
+MODULE_PATH = "/Users/antonoeschmidt/PycharmProjects/solitaire-yolo/__init__.py"
+# MODULE_PATH = "/home/antonio/solitaire-yolo/__init__.py"
+MODULE_NAME = "card1"
+import importlib.util
+import sys
+spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
+module = importlib.util.module_from_spec(spec)
+sys.modules[spec.name] = module
+spec.loader.exec_module(module)
+from card1 import card
--- yolo/yolo3.py
+++ yolo/yolo3.py
@@ -4,16 +4,15 @@ import time
 # from card import Card
 
 # MODULE_PATH = "/Users/antonoeschmidt/PycharmProjects/solitaire-yolo/__init__.py"
-MODULE_PATH = "/home/antonio/solitaire-yolo/__init__.py"
-MODULE_NAME = "card1"
-import importlib.util
-import sys
-spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
-module = importlib.util.module_from_spec(spec)
-sys.modules[spec.name] = module
-spec.loader.exec_module(module)
+# MODULE_PATH = "/home/antonio/solitaire-yolo/__init__.py"
+# MODULE_NAME = "card1"
+# import importlib.util
+# import sys
+# spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
+# module = importlib.util.module_from_spec(spec)
+# sys.modules[spec.name] = module
+# spec.loader.exec_module(module)
 
-from card1 import card
 from card import Card
 
 def detect(image_BGR, picNumber, debug='no'):
