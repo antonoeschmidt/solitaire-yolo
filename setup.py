8163e9bd358a73c8ca06f60b03cecd5193a0e632
antonoeschmidt <s163053@student.dtu.dk>
2020-06-24 17:55:18 +0200

    Imports and init.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='application',
-    packages=['application', 'main'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
